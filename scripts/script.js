02b379d39c1bd5840f4dcf276ebc6dc2bc285c36
kenjiw360 <60027724+kenjiw360@users.noreply.github.com>
2020-01-19 02:32:32 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+/*window.onscroll = function() {myFunction()};
 
 // Get the header
 var header = document.getElementById("myHeader");
@@ -15,7 +15,7 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
-
+*/
 //everything above is Muhammad
 
 
