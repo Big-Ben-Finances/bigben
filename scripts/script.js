180b9e7fc5ee2e00eb286ccc5d8b7824a2ea6899
kenjiw360 <60027724+kenjiw360@users.noreply.github.com>
2020-01-19 01:01:58 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function goSomewhere(x){
-  window.location = x;
+window.location = x;
 }
 function startOff(){
 if(localStorage.getItem("loggedIn") == "true"){
@@ -9,4 +9,18 @@ document.getElementById("changeToName").innerHTML = "<h2 class='text' style='flo
 function reset(){
 localStorage.setItem("loggedIn",null);
 window.location = "/bigben/index.html";
+}
+function changeDescription(){
+document.getElementById("descriptionr").innerHTML = "<textarea class='text' style='margin: 0px; width: 994px; height: 137px;font-size:20px;' id='textr' placeholder='Edit Description'></textarea>"+"<br><button class='loginandsignupconfirm' onclick='descriptionSave()'>Save</button>";
+}
+function startOff(){
+if(localStorage.getItem("loggedIn") == "true"){
+document.getElementById("changeToName").innerHTML = "<h2 class='text' style='float:right;'>"+localStorage.getItem('username')+"</h2><button class='loginandsignupconfirm' onclick='reset()' style='float:right;background-color:rgb(115,243,195);'>Log out</button>";
+document.getElementById("name").innerHTML = localStorage.getItem("firstname")+" "+ localStorage.getItem("lastname");
+if(localStorage.getItem("description")){
+document.getElementById("descriptionr").innerHTML = "<p class = 'text' style='font-size:20px;'>"+localStorage.getItem("description")+"</p>"+"<br><button class='loginandsignupconfirm' onclick='changeDescription()'>Edit Description</button>";
+}else{
+document.getElementById("descriptionr").innerHTML = "<textarea class='text' style='margin: 0px; width: 994px; height: 137px;font-size:20px;' id='textr' placeholder='Edit Description'></textarea>"+"<br><button class='loginandsignupconfirm' onclick='descriptionSave()'>Save</button>";
+}
+}
 }
